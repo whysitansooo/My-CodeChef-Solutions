022ede292d89456f92a9c2b278dd9f52c700785b
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2023-04-29 23:15:34 +0530

    Create GoodTurn.c

---
+++ GoodTurn.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int t,x,y;
+	scanf("%d",&t);
+	for(int i=1;i<=t;i++){
+	    scanf("%d %d",&x,&y);
+	    	if(x+y>6){
+	    printf("YES\n");
+	   
+	}
+	else{
+	    printf("NO\n");
+	    
+	    
+	}
+	}
+
+	return 0;
+}
