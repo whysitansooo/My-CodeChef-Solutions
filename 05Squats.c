1bc2861f901c4262c12d4be11d1086e74e451f53
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2023-05-01 21:02:29 +0530

    Difficulty Rating: 2 49

---
+++ 05Squats.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int T;
+	scanf("%d",&T);
+	while(T--){
+	    int S;
+	    scanf("%d",&S);
+	    printf("%d\n",S*15);
+	}
+	return 0;
+}
